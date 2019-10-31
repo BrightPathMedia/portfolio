275d6dab5059ddd56df08ba7c6bdd80bc1cdd338
Abhishek R <abhishek@x47d.com>
2019-10-31 14:25:50 +0530

    Fix work page mobile css

--- src/pages/work/[id].vue
+++ src/pages/work/[id].vue
@@ -2,11 +2,11 @@
   <Layout>
 
     <div class='flex flex-col lg:flex-row my-4 w-full'>
-      <div class='w-1/2 mr-8'>
+      <div class='w-full lg:w-1/2 p-4 lg:p-0 lg:mr-8'>
         <img :src="currentItem.image" class="h-auto w-auto" />
       </div>
 
-      <div class='w-1/2 ml-8 border-t pt-2 border-black'>
+      <div class='w-auto lg:w-1/2 ml-4 lg:m-0 lg:ml-8 border-t pt-2 border-black order-first lg:order-none'>
         <h1 class='h1 text-2xl font-bold mb-4'>{{ currentItem.name }}</h1>
         <p class='desc'>
           {{ currentItem.desc }}
@@ -33,7 +33,6 @@ export default {
   async mounted() {
     const { id } = this.$route.params
     for(var key in this.$t('work.workItems')) {
-      console.log(this.$t('work.workItems')[key])
       var w = this.$t('work.workItems')[key]
       if (w.url.replace('/work/','').toLowerCase() == id.toLowerCase()) {
         this.currentItemIndex = key
