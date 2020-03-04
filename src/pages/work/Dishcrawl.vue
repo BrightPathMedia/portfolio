03c8df4dc0bb1ea7eeec18f8eb5e0d866b51cd43
James Bruno <jamesbruno@jemuzunoMacBook-Pro.local>
2020-03-04 09:47:58 +0900

    Adding in Lately

--- src/pages/work/Dishcrawl.vue
+++ src/pages/work/Dishcrawl.vue
@@ -4,28 +4,28 @@
       div(class="lg:border-t").pt-2.border-black.order-first.w-auto.ml-4.w-half.portfolitem.portfolitext
         h1.text-2xl.font-bold.mb-4 {{ currentItem.name }}
         p.desc {{ currentItem.desc }}
-    .portfolirow
-      img(:src='currentItem.images.cover').portfolitem
     .portfolirow.flex-row-reverse.just-right
       .portfolitext.w-half.portfolitem
         h1.portfolisubhead {{ currentItem.bio["1-title"] }}
         p {{ currentItem.bio[1] }}
     .portfolirow
-      img(:src='currentItem.images.cover').portfolitem
+      img(:src='currentItem.images.homepage1').portfolitem.w-half
+      img(:src='currentItem.images.homepage2').portfolitem.w-half
+    .portfolirow.flex-row-reverse.just-right
+      .portfolitext.w-half.portfolitem
+        h1.portfolisubhead {{ currentItem.bio["battledishTitle"] }}
+        p {{ currentItem.bio["battledishBody"] }}
+    .portfolirow
+      img(:src='currentItem.images.battledish').portfolitem
     .portfoligroup
       .portfolirow.flex-row-reverse
-        img( :src='currentItem.images.cover').portfolitem.w-half
-        .w-half.portfolitext.portfolitem.order-first
-          h1.portfolisubhead {{ currentItem.bio["2-title"] }}
-          p {{ currentItem.bio[2] }}
+        img(:src='currentItem.images.tastemakersLogo').portfolitem.w-half
+        .portfolitext.w-half.portfolitem.order-first
+          h1.portfolisubhead {{ currentItem.bio["tastemakersTitle"] }}
+          p {{ currentItem.bio["tastemakersBody"] }}
       .portfolirow
-        img( :src='currentItem.images.cover').portfolitem.w-half
-        img( :src='currentItem.images.cover').portfolitem.w-half
-    .portfoligroup
-      .portfolirow
-        img( :src='currentItem.images.cover').w-full.portfolitem
-    .portfolirow
-      img(:src='currentItem.images.cover').portfolitem
+        img(:src='currentItem.images.tastemakersCombo').portfolitem
+    
 </template>
 
 <script>
---
+++ src/pages/work/Lately.vue
@@ -0,0 +1,34 @@
+<template lang=pug>
+  Portfolio
+    .portfolirow.flex-row-reverse.just-right
+      div(class="lg:border-t").pt-2.border-black.order-first.w-auto.ml-4.w-half.portfolitem.portfolitext
+        h1.text-2xl.font-bold.mb-4 {{ currentItem.name }}
+        p.desc {{ currentItem.desc }}
+    .portfolirow
+      img(:src='currentItem.images.straight').portfolitem.w-half
+      img(:src='currentItem.images.curly').portfolitem.w-half
+    
+</template>
+
+<script>
+import Vue from "vue";
+import Portfolio from "../../layouts/Portfolio.vue";
+import Component from "vue-class-component";
+
+@Component({
+  components: {
+    Portfolio
+  }
+})
+export default class Lately extends Vue {
+  test = 20;
+  currentItemIndex = 4;
+
+  get currentItem() {
+    return this.$t("work.workItems")[this.currentItemIndex];
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
