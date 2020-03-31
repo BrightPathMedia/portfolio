4bd276614e2f88a99bdc0ec5398479e1520f98a6
Jeiji <jeiji@me.com>
2020-04-01 08:55:25 +0900

    Added Entry template for work entry classes

---
+++ src/pages/work/Entry.vue
@@ -0,0 +1,20 @@
+<script>
+import Vue from "vue";
+
+export default class Entry extends Vue {
+  get currentItem() {
+    return this.$t("work.workItems.rows")["r" + this.currentRow][
+      this.currentItemIndex
+    ];
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.work-item {
+  .title {
+    font-weight: 600;
+    font-size: 1rem;
+  }
+}
+</style>
